a7aef996c01d18bb344b0fe227ed2d057ff7ac57
Polar Humenn <polar@greene.case.syr.edu>
2009-04-19 13:02:29 -0400

    Added Textile Help Links to Edit Page Form

--- engine_plugins/paginating_find/init.rb
+++ engine_plugins/paginating_find/init.rb
@@ -2,3 +2,5 @@ require 'paginating_find.rb'
 
 ActiveRecord::Base.send(:include, PaginatingFind)
 ActionView::Base.send(:include, PaginatingFind::Helpers)
+
+puts "THIS IS THE Comatose Engine PaginatingFind Plugin"
